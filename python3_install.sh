4f02a11ffa0b9e465c44af3740cc8894e10d3be9
ahugla <ahugla@vmware.com>
2020-01-06 14:34:27 +0100

    Create python3_install.sh

---
+++ python3_install.sh
@@ -0,0 +1,11 @@
+# install python3 et pip3 on CentOS7
+#------------------------------------
+
+yum install -y python36 python-pip
+
+yum install python36-setuptools
+
+easy_install-3.6 pip
+
+python3 -V
+pip3 -V
